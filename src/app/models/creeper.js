991ca4d8f7406f8ea5493e36f507f52f0d19e5bd
Nayotama Pradipta <n4y0t4m4@gmail.com>
2023-04-20 14:07:10 +0700

    feat: add creeper model

---
+++ src/app/models/creeper.js
@@ -0,0 +1,76 @@
+export default {
+    RBody: {
+        children: {
+            PBody: {
+                options: {
+                    position: [0,1,0],
+                    scale: [0.5, 2, 1],
+                },
+            },
+            RHead: {
+                options: {
+                    position: [0, 2, 0],
+                },
+                children: {
+                    PHead: {
+                        options: {
+                            position: [0, 0.5, 0],
+                        },
+                    },
+                },
+            },
+            RLegFR: {
+                options: {
+                    position: [0.25, 0, -0.25],
+                },
+                children: {
+                    PLegFR: {
+                        options: {
+                            position: [0.25, -0.5, 0],
+                            scale: [0.5, 1, 0.5],
+                        },
+                    },
+                },
+            },
+            RLegFL: {
+                options: {
+                    position: [0.25, 0, 0.25],
+                },
+                children: {
+                    PLegFL: {
+                        options: {
+                            position: [0.25, -0.5, 0],
+                            scale: [0.5, 1, 0.5],
+                        },
+                    },
+                },
+            },
+            RLegBR: {
+                options: {
+                    position: [-0.25, 0, -0.25],
+                },
+                children: {
+                    PLegBR: {
+                        options: {
+                            position: [-0.25, -0.5, 0],
+                            scale: [0.5, 1, 0.5],
+                        },
+                    },
+                },
+            },
+            RLegBL: {
+                options: {
+                    position: [-0.25, 0, 0.25],
+                },
+                children: {
+                    PLegBL: {
+                        options: {
+                            position: [-0.25, -0.5, 0],
+                            scale: [0.5, 1, 0.5],
+                        },
+                    },
+                },
+            },
+        }
+    }
+}
