b090029927a24778cc38a21eb8092b6a3ead2c5a
Nayotama Pradipta <n4y0t4m4@gmail.com>
2023-04-20 21:32:15 +0700

    feat: squid animation

---
+++ src/app/models/squidanimation.js
@@ -0,0 +1,315 @@
+export default {
+    name: "Swim",
+    frames: [
+        // 0 
+        {
+            RBody: {
+                position: [0,0.2,0],
+                rotation: [0,15,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0,0,0],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0,0,0],
+            }
+
+        },
+        // 1
+        {
+            RBody: {
+                position: [0,0.4,0],
+                rotation: [0,30,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [15, 0, 15],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [-15, 0, 15],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [-15, 0, -15],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [15, 0, -15],
+            }
+        },
+        // 2
+        {
+            RBody: {
+                position: [0,0.6,0],
+                rotation: [0,45,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [15, 0, 0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0, 0, 15],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [-15, 0, 0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0, 0, -15],
+            }
+        },
+        // 3
+        {
+            RBody: {
+                position: [0,0.8,0],
+                rotation: [0,60,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [30, 0, 30],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [-30, 0, 30],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [-30, 0, -30],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [30, 0, -30],
+            }
+        },
+        // 4
+        {
+            RBody: {
+                position: [0,1,0],
+                rotation: [0,75,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [30, 0, 0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0, 0, 30],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [-30, 0, 0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0, 0, -30],
+            }
+        },
+        // 5
+        {
+            RBody: {
+                position: [0,1.2,0],
+                rotation: [0,90,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [45, 0, 45],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [-45, 0, 45],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [-45, 0, -45],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [45, 0, -45],
+            }
+        },
+        // 6
+        {
+            RBody: {
+                position: [0,1.4,0],
+                rotation: [0,105,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [45, 0, 0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0, 0, 45],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [-45, 0, 0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0, 0, -45],
+            }
+        },
+        // 7
+        {
+            RBody: {
+                position: [0,1.4,0],
+                rotation: [0,120,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [30, 0, 30],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [-30, 0, 30],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [-30, 0, -30],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [30, 0, -30],
+            }
+        },
+        // 8
+        {
+            RBody: {
+                position: [0,1.6,0],
+                rotation: [0,135,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [30, 0, 0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0, 0, 30],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [-30, 0, 0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0, 0, -30],
+            }
+        },
+        // 9
+        {
+            RBody: {
+                position: [0,1.8,0],
+                rotation: [0,150,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [15, 0, 15],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [-15, 0, 15],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [-15, 0, -15],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [15, 0, -15],
+            }
+        },
+        // 10
+        {
+            RBody: {
+                position: [0,2,0],
+                rotation: [0,165,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [15, 0, 0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0, 0, 15],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [-15, 0, 0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0, 0, -15],
+            }
+        },
+        // 1 
+        {
+            RBody: {
+                position: [0,2.1,0],
+                rotation: [0,180,0],
+            },
+            RTentacle1: {
+                position: [0.5, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle2: {
+                position: [0.5, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle3: {
+                position: [-0.5, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle4: {
+                position: [-0.5, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle5: {
+                position: [0, -0.75, -0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle6: {
+                position: [0.5, -0.75, 0],
+                rotation: [0,0,0],
+            },
+            RTentacle7: {
+                position: [0, -0.75, 0.5],
+                rotation: [0,0,0],
+            },
+            RTentacle8: {
+                position: [-0.5, -0.75, 0],
+                rotation: [0,0,0],
+            }
+
+        },
+    ]
+}
